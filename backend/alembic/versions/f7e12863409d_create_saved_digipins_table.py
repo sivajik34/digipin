1954db4f6dd29b0f28f87eed1959701b1fcbfb73
Sivakumar Koduru <sivajik34@gmail.com>
2025-06-07 19:57:09 +0530

    fix

--- backend/alembic/versions/f7e12863409d_create_saved_digipins_table.py
+++ backend/alembic/versions/f7e12863409d_create_saved_digipins_table.py
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy import inspect
 
 # revision identifiers, used by Alembic.
 revision: str = 'f7e12863409d'
