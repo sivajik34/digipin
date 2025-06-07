9d73acae4f6413ed3a1e439fa9837a5270c4ce20
Sivakumar Koduru <sivajik34@gmail.com>
2025-06-07 13:16:45 +0530

    map added

---
+++ frontend/src/components/LocationMap.js
@@ -0,0 +1,39 @@
+// src/components/LocationMap.js
+import React from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix missing marker icons
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+const ClickHandler = ({ onClick }) => {
+  useMapEvents({
+    click(e) {
+      const { lat, lng } = e.latlng;
+      onClick(lat, lng);
+    },
+  });
+  return null;
+};
+
+const LocationMap = ({ onLocationSelect, marker }) => {
+  return (
+    <MapContainer center={[20.5937, 78.9629]} zoom={5} style={{ height: "400px", width: "100%" }}>
+      <TileLayer
+        attribution="&copy; OpenStreetMap contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <ClickHandler onClick={onLocationSelect} />
+      {marker && <Marker position={[marker.lat, marker.lng]} />}
+    </MapContainer>
+  );
+};
+
+export default LocationMap;
