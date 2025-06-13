e25e6d58d2dd9e321b2c0cc89795f540f8c86706
Koduru sivakumar <koduru.sivakumar@globallogic.com>
2025-06-13 11:07:55 +0530

    map width fix

--- frontend/src/components/GetDigipin.js
+++ frontend/src/components/GetDigipin.js
@@ -115,7 +115,7 @@ const GetDigipin = ({ isLoggedIn }) => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-4 space-y-6">     
+    <div className="w-full p-4 space-y-6">     
 
       <form
         onSubmit={handleSubmit}
